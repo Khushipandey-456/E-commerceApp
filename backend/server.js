1db68dcdda69c4d841cc9d31ad516fcaf4e92b7b
Khushipandey-456 <kp9761569@gmail.com>
2025-03-18 19:55:21 +0530

    addedNew

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
