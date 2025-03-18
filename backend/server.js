fc7c8d7baa420f7d1ef43b5a028862b3e0af2807
Khushipandey-456 <kp9761569@gmail.com>
2025-03-18 19:53:27 +0530

    Addednew

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const productRoutes = require('../scripts/routes/product.route');
 const path = require('path');
